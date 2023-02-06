480a281509cc424a5223823d4bcb1906b784a1bd
Wissam Mansour <wissman77@gmail.com>
2023-02-06 11:49:28 +0200

    remove html-wepack plugin and server

---
+++ bundlef0ebfe2893004ff57896.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('webpack test passed!');
+
+/******/ })()
+;
+//# sourceMappingURL=bundlef0ebfe2893004ff57896.js.map
