f5752dcec9b79f7281428662aebb6549032c251e
Wissam Mansour <wissman77@gmail.com>
2023-02-06 11:49:28 +0200

    remove html-wepack plugin and server

--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,16 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'developement',
-  entry: './src/index.js',
+  entry: {
+    bundle: path.resolve(__dirname, 'src/index.js'),
+  },
   output: {
-    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    filename: '[name][contenthash].js',
     clean: true,
     assetModuleFilename: '[name][ext]',
   },
-  devtool: 'inline-source-map',
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, 'dist'),
-    },
-    port: 3000,
-    open: true,
-    hot: true,
-    compress: true,
-    historyApiFallback: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Resturant Page',
-    }),
-  ],
+  devtool: 'source-map',
   module: {
     rules: [
       {
