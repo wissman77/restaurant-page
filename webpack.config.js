7af522c709f2457d3cde3704eb9f8431ca881bbb
Wissam Mansour <wissman77@gmail.com>
2023-02-06 12:11:25 +0200

    change to basic config

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,11 @@
 const path = require('path');
 
 module.exports = {
-  entry: {
-    bundle: path.resolve(__dirname, 'src/index.js'),
-  },
+  mode: 'development',
+  entry: './src/index.js',
   output: {
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name][contenthash].js',
-    clean: true,
-    assetModuleFilename: '[name][ext]',
   },
   devtool: 'source-map',
   module: {
