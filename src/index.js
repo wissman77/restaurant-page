adbe16463861591ac929af712e3aec93f94953f2
Wissam Mansour <wissman77@gmail.com>
2023-02-07 10:14:29 +0200

    imports all modules and create function pageLoad also used by navbar module click events

--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
-console.log('webpack test passed!');
+import createAbout from './modules/about';
+import createContact from './modules/contact';
+import createFooter from './modules/footer';
+import createHeader from './modules/header';
+import createHomePage from './modules/home';
+import createMenu from './modules/menu';
+import createNavbar from './modules/navbar';
+
+const HOME = 'Home';
+const MENU = 'Menu';
+const ABOUT = 'About';
+const CONTACT = 'Contact';
+
+const mainModule = (() => {
+  const pageLoad = (page) => {
+    const content = document.querySelector('#content');
+    content.innerHTML = '';
+    const header = createHeader();
+    content.append(header);
+    const navbar = createNavbar(page);
+    content.appendChild(navbar);
+
+    switch (page) {
+      case HOME:
+        content.appendChild(createHomePage());
+        break;
+      case MENU:
+        content.appendChild(createMenu());
+        break;
+      case ABOUT:
+        content.appendChild(createAbout());
+        break;
+      case CONTACT:
+        content.appendChild(createContact());
+        break;
+    }
+
+    const footer = createFooter();
+    content.appendChild(footer);
+  };
+
+  return {
+    pageLoad,
+  };
+})();
+
+mainModule.pageLoad(HOME);
+
+export default mainModule;
